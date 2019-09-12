81e4cdc062140f058bd3b035126f4f9b450475f7
Trikolon <git@pbz.im>
2019-09-12 22:45:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const Keyv = require('keyv');
+
+const config = require('./config.json');
+
+let userDB;
+
+async function toggleMention(user) {
+  if (await userDB.get(user.id.toString())) {
+    await userDB.delete(user.id.toString());
+    return false;
+  }
+  await userDB.set(user.id.toString(), true);
+  return true;
+}
+
+async function commandHandler(message) {
+  const state = await toggleMention(message.author);
+  return message.reply(`Ping warning **${state ? 'enabled' : 'disabled'}**.`);
+}
+
+async function getAffectedUsers(message) {
+  const affectedUsers = message.mentions.members.map(async (user) => {
+    if (await userDB.get(user.id.toString())) {
+      return user;
+    }
+    return false;
+  });
+  return (await Promise.all(affectedUsers)).filter((result) => !!result);
+}
+
+function usersToString(users) {
+  return users.reduce((str, user) => `${str} ${user instanceof Discord.GuildMember ? user.displayName : user.username},`, '')
+    .slice(0, -1);
+}
+
+async function chatHandler(message) {
+  const affectedUsers = await getAffectedUsers(message);
+
+  if (affectedUsers.length > 0) {
+    let userPrefix;
+    if (affectedUsers.length === 1) {
+      userPrefix = `**${usersToString(affectedUsers)}** has`;
+    } else {
+      userPrefix = `**${usersToString(affectedUsers)}** have`;
+    }
+
+    if (!config.action.deleteMessage && config.action.reactEmoji) {
+      await message.react(config.action.reactEmoji);
+    }
+    await message.reply(`${userPrefix} mentions disabled. Please don't ping them!`);
+    if (config.action.deleteMessage && message.deletable) {
+      await message.delete();
+    }
+  }
+}
+
+let botToken = process.env.BOTTOKEN;
+if (!botToken) {
+  botToken = config && config.api && config.api.token;
+}
+
+if (!botToken) {
+  console.error('Missing bot token! Please add it to config.json');
+  process.exit(1);
+}
+
+client.on('error', (e) => console.error(e));
+client.on('warn', (e) => console.warn(e));
+
+client.on('ready', () => {
+  console.info(`Logged in as ${client.user.tag}!`);
+  if (config.api.appId) {
+    console.info(
+      `Add this bot to a server: https://discordapp.com/oauth2/authorize?client_id=${config.api.appId}&scope=bot`,
+    );
+  }
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content.startsWith(config.command.prefix)) {
+    commandHandler(message);
+    return;
+  }
+
+  chatHandler(message);
+});
+
+userDB = new Keyv(config.database, { namespace: 'users' });
+userDB.on('error', (err) => {
+  console.error('Database connection error:', err);
+  process.exit(1);
+});
+
+
+client.login(botToken)
+  .catch((error) => {
+    console.error('Error autenticating with Discord! Check your bot token.', error.message);
+    console.debug(error);
+    process.exit(1);
+  });
